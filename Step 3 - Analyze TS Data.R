677f1664e063898597ad368ed07e7ac1a93b8bdb
Chris Kiniry <chriskiniry@gmail.com>
2018-01-23 17:25:52 -0600

    Late in day.

--- Step 3 - Analyze TS Data.R
+++ Step 3 - Analyze TS Data.R
@@ -18,26 +18,74 @@ big_property_data <- read_rds("big_property_data.rds")
 
 
 
-# Submarket - Eff Rent TS -------------------------------------------------
+# Prop Count --------------------------------------------------------------
+
+# Add a column with a bunch of 1's that can be summed each time to get a prop count
+big_property_data <- big_property_data %>%
+                     mutate(prop_count = 1)
 
 
+
+# Submarket - Sequential Monthly Eff Rent TS -------------------------------------------------
+
+# Calculate Eff Rent using the comparable_monthly_mom filter
 test <- big_property_data %>%
-        filter(comparable_monthly_MOM == TRUE) %>%
-        group_by(Submarket, Date) %>%
-        summarize(eff_rent = sum( total_rent ) / sum( Quantity) )
+        filter(comparable_monthly_mom == TRUE) %>%
+        group_by(submarket, date) %>%
+        summarize(prop_count = sum(prop_count),
+                  eff_rent = sum( total_rent ) / sum( quantity) )
+
+
+# This will format the data back into a human readable layout
+test %>%
+  filter(date > ymd("2017-08-01")) %>%
+  spread(key = date, value = eff_rent) %>%
+  View()
+
+
+# This 
+test %>%
+  filter(submarket == "Carmel",
+         year(date) >= 2016) %>%
+  spread(key = date, value = eff_rent) %>%
+  write_csv("test.csv")
+
+
+
+
+# Submarket - Sequential Quarterly Eff Rent TS ----------------------------
+
+
+# Calculate Eff Rent using the comparable_monthly_mom filter
+test <- big_property_data %>%
+        filter(comparable_qtr_qoq == TRUE) %>%
+        group_by(submarket, current_quarter) %>%
+        summarize(prop_count = sum(prop_count) / 3,
+                  eff_rent = round( sum( total_rent ) / sum( quantity) , 0 ) )
+
+test %>%
+  filter(current_quarter > 2017) %>%
+  spread(key = current_quarter, value = eff_rent) %>%
+  View()
 
 
 
 
 
+# Submarket - Eff Rent Quartely YOY ---------------------------------------
 
 
+# Calculate Eff Rent using the comparable_monthly_mom filter
+test <- big_property_data %>%
+        filter(comparable_qtr_yoy == TRUE) %>%
+        group_by(submarket, current_quarter) %>%
+        summarize(prop_count = sum(prop_count) / 3,
+                  eff_rent = round( sum( total_rent ) / sum( quantity) , 0 ) )
 
-
-
-
-
-
+test %>%
+  filter(current_quarter > 2017) %>%
+  spread(key = current_quarter, value = eff_rent) %>%
+  View()
 
 
 
